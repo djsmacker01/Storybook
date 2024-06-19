fb690e1fa8ac0b0dd378b5541aa10992fd45bf0e
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-06-19 01:46:59 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const app = express();
+
 //load configuration
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: './config/config.env' })
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server is listening on ${process.env.NODE_ENV} mode on port ${PORT}`));
