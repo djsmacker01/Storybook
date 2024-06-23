df20dfb62d295f221555bcb281b4baf9ef181209
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-06-23 11:18:13 +0100

    new update on index.js

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,12 @@ router.get('/', (req, res) => {
     res.send('Login')
 })
 
+//@des  Dashboard
+//@route GET/dashboard
+
+router.get('/dashboard', (req, res) => { 
+    res.send('Dashboard')
+})
 
 
 module.exports = router
