76d418db25faf5a29445fa6e6d5f8f9d0f6d47cf
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-06-23 17:38:09 +0100

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,14 @@ const router = express.Router();
 //@route GET/
 
 router.get('/', (req, res) => { 
-    res.send('Login')
+    res.render('login')
 })
 
 //@des  Dashboard
 //@route GET/dashboard
 
 router.get('/dashboard', (req, res) => { 
-    res.send('Dashboard')
+    res.render('dashboard')
 })
 
 
