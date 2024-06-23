11967541fde3465c8a23d39ed064bf7092ee8cf3
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-06-23 19:26:34 +0100

    update app.js

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db')
 
 //load configuration
 dotenv.config({ path: './config/.env' });
 
+//passport config
+require('./config/passport')(passport)
 connectDB()
 
 const app = express();
@@ -22,6 +26,18 @@ if (process.env.NODE_ENV === 'development') {
 // app.set('view engine', '.hbs')
 app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  // cookie: {secure:true}
+}))
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // Static folder
 app.use('/public',express.static(path.join(__dirname, 'public')))
