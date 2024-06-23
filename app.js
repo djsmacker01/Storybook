f209b301d1420307d7032b642c8c569b6775fbdb
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-06-23 20:13:09 +0100

    new commit

--- app.js
+++ app.js
@@ -44,6 +44,8 @@ app.use('/public',express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+
 
 const PORT = process.env.PORT || 3000;
 
---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+//@des auth with google
+//@route GET/auth/google
+
+router.get('/google', passport.authenticate('google', {scope:['profile']}))
+
+//@des  Google auth callback
+//@route GET/auth/google/callback
+
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/dashboard');
+})
+
+
+module.exports = router
