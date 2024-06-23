8edb55347ffd066531859b7f2b9e0716197bf2b0
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-06-23 11:09:22 +0100

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars');
 const connectDB = require('./config/db')
 
 //load configuration
@@ -15,6 +15,12 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
+
+//Handlebars
+// app.engine('.hbs',exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+// app.set('view engine', '.hbs')
+app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT || 3000;
 
